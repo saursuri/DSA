529989b2bc50a94238e27e9292b63c3c8f02b63e
Saurav Suri <60064237+saursuri@users.noreply.github.com>
2024-10-11 23:31:31 +0530

    1942. The Number of the Smallest Unoccupied Chair

---
+++ LeetCode_Questions/Priority_Queue/1942. The Number of the Smallest Unoccupied Chair/Code.cpp
@@ -0,0 +1,54 @@
+/*
+Problem Link: https://leetcode.com/problems/the-number-of-the-smallest-unoccupied-chair/description/
+Time Complexity : O(nlogn)
+Space Complexity : O(n)
+*/
+
+class Solution {
+public:
+    typedef pair<int, int> P;
+
+    int smallestChair(vector<vector<int>>& times, int targetFriend) {
+        
+        int n = times.size();
+        priority_queue<P, vector<P>, greater<P> > occupied; //{departTime, chairNo}
+        priority_queue<int, vector<int>, greater<int>> free; //min heap of unoccupied chairs
+        
+        int targetFriendArrival = times[targetFriend][0];
+		
+        //Sort based on arrival time
+        sort(times.begin(),times.end());
+        
+		int chairNo = 0;
+
+        for(int i = 0; i < n; i++) {
+            int arrival  = times[i][0];
+            int depart   = times[i][1];
+            
+            //free chairs accordingly
+            while(!occupied.empty() && occupied.top().first <= arrival) {
+                free.push(occupied.top().second); //this chair is now free
+                occupied.pop();
+            }
+
+            if(free.empty()) {
+                occupied.push({depart, chairNo});
+
+                if(arrival == targetFriendArrival)
+                    return chairNo;
+
+                chairNo++;
+            } else {
+                int leastChairAvailable = free.top();
+                free.pop();
+                if(arrival == targetFriendArrival) {
+                    return leastChairAvailable;
+                }
+                occupied.push({depart, leastChairAvailable});
+            }
+        }
+
+        return -1;
+    }
+    
+};
